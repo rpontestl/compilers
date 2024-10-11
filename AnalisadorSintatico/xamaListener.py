6f4e3244707b9bf8b88266a76f7ef63f5f829213
rafapontestl <90840183+rpontestl@users.noreply.github.com>
2024-10-10 22:05:34 -0300

    xamaListener

--- AnalisadorSintatico/xamaListener.py
+++ AnalisadorSintatico/xamaListener.py
@@ -1,228 +1,73 @@
 # Generated from xama.g4 by ANTLR 4.13.2
 from antlr4 import *
+from collections import defaultdict
+
 if "." in __name__:
     from .xamaParser import xamaParser
 else:
     from xamaParser import xamaParser
 
-# This class defines a complete listener for a parse tree produced by xamaParser.
+class SymbolTable:
+    def __init__(self):
+        self.symbols = defaultdict(dict)
+        self.current_scope = None
+
+    def enter_scope(self, name):
+        self.current_scope = name
+
+    def exit_scope(self):
+        self.current_scope = None
+
+    def add_symbol(self, name, type_):
+        if self.current_scope:
+            self.symbols[self.current_scope][name] = type_
+        else:
+            self.symbols[name] = type_
+
+    def lookup(self, name):
+        if self.current_scope and name in self.symbols[self.current_scope]:
+            return self.symbols[self.current_scope][name]
+        return self.symbols.get(name)
+
+symbol_table = SymbolTable()
+
 class xamaListener(ParseTreeListener):
-
-    # Enter a parse tree produced by xamaParser#program.
-    def enterProgram(self, ctx:xamaParser.ProgramContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#program.
-    def exitProgram(self, ctx:xamaParser.ProgramContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#decl_list.
-    def enterDecl_list(self, ctx:xamaParser.Decl_listContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#decl_list.
-    def exitDecl_list(self, ctx:xamaParser.Decl_listContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#decl.
-    def enterDecl(self, ctx:xamaParser.DeclContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#decl.
-    def exitDecl(self, ctx:xamaParser.DeclContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#var_decl.
     def enterVar_decl(self, ctx:xamaParser.Var_declContext):
-        pass
+        type_ = ctx.type_().getText()
+        ids = ctx.id_list().getText().split(',')
+        for id_ in ids:
+            if symbol_table.lookup(id_):
+                print(f"Erro: Variável '{id_}' já declarada")
+            else:
+                symbol_table.add_symbol(id_, type_)
+                print(f"Declaração de variável: {id_} do tipo {type_}")
 
-    # Exit a parse tree produced by xamaParser#var_decl.
-    def exitVar_decl(self, ctx:xamaParser.Var_declContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#const_decl.
     def enterConst_decl(self, ctx:xamaParser.Const_declContext):
-        pass
+        type_ = ctx.type_().getText()
+        id_ = ctx.ID().getText()
+        if symbol_table.lookup(id_):
+            print(f"Erro: Constante '{id_}' já declarada")
+        else:
+            symbol_table.add_symbol(id_, type_)
+            print(f"Declaração de constante: {id_} do tipo {type_}")
 
-    # Exit a parse tree produced by xamaParser#const_decl.
-    def exitConst_decl(self, ctx:xamaParser.Const_declContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#type.
-    def enterType(self, ctx:xamaParser.TypeContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#type.
-    def exitType(self, ctx:xamaParser.TypeContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#id_list.
-    def enterId_list(self, ctx:xamaParser.Id_listContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#id_list.
-    def exitId_list(self, ctx:xamaParser.Id_listContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#type_decl.
-    def enterType_decl(self, ctx:xamaParser.Type_declContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#type_decl.
-    def exitType_decl(self, ctx:xamaParser.Type_declContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#func_decl.
     def enterFunc_decl(self, ctx:xamaParser.Func_declContext):
-        pass
+        id_ = ctx.ID().getText()
+        return_type = ctx.type_().getText()
+        param_list = ctx.param_list().getText()
+        # Para simplificação, estamos apenas adicionando a função sem verificar os parâmetros agora.
+        if symbol_table.lookup(id_):
+            print(f"Erro: Função '{id_}' já declarada")
+        else:
+            symbol_table.add_symbol(id_, f"function({param_list}): {return_type}")
+            print(f"Declaração de função: {id_} retornando {return_type} com parâmetros {param_list}")
 
-    # Exit a parse tree produced by xamaParser#func_decl.
-    def exitFunc_decl(self, ctx:xamaParser.Func_declContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#param_list.
-    def enterParam_list(self, ctx:xamaParser.Param_listContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#param_list.
-    def exitParam_list(self, ctx:xamaParser.Param_listContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#block.
-    def enterBlock(self, ctx:xamaParser.BlockContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#block.
-    def exitBlock(self, ctx:xamaParser.BlockContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#var_const_list.
-    def enterVar_const_list(self, ctx:xamaParser.Var_const_listContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#var_const_list.
-    def exitVar_const_list(self, ctx:xamaParser.Var_const_listContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#stmt_list.
-    def enterStmt_list(self, ctx:xamaParser.Stmt_listContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#stmt_list.
-    def exitStmt_list(self, ctx:xamaParser.Stmt_listContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#stmt.
-    def enterStmt(self, ctx:xamaParser.StmtContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#stmt.
-    def exitStmt(self, ctx:xamaParser.StmtContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#print_stmt.
     def enterPrint_stmt(self, ctx:xamaParser.Print_stmtContext):
-        pass
+        print("Comando de impressão")
 
-    # Exit a parse tree produced by xamaParser#print_stmt.
-    def exitPrint_stmt(self, ctx:xamaParser.Print_stmtContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#expr.
-    def enterExpr(self, ctx:xamaParser.ExprContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#expr.
-    def exitExpr(self, ctx:xamaParser.ExprContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#cond.
-    def enterCond(self, ctx:xamaParser.CondContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#cond.
-    def exitCond(self, ctx:xamaParser.CondContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#bitwise.
-    def enterBitwise(self, ctx:xamaParser.BitwiseContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#bitwise.
-    def exitBitwise(self, ctx:xamaParser.BitwiseContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#relational.
-    def enterRelational(self, ctx:xamaParser.RelationalContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#relational.
-    def exitRelational(self, ctx:xamaParser.RelationalContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#shift.
-    def enterShift(self, ctx:xamaParser.ShiftContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#shift.
-    def exitShift(self, ctx:xamaParser.ShiftContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#add_sub.
-    def enterAdd_sub(self, ctx:xamaParser.Add_subContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#add_sub.
-    def exitAdd_sub(self, ctx:xamaParser.Add_subContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#mul_div.
-    def enterMul_div(self, ctx:xamaParser.Mul_divContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#mul_div.
-    def exitMul_div(self, ctx:xamaParser.Mul_divContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#expr_list.
-    def enterExpr_list(self, ctx:xamaParser.Expr_listContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#expr_list.
-    def exitExpr_list(self, ctx:xamaParser.Expr_listContext):
-        pass
-
-
-    # Enter a parse tree produced by xamaParser#lv.
     def enterLv(self, ctx:xamaParser.LvContext):
-        pass
-
-    # Exit a parse tree produced by xamaParser#lv.
-    def exitLv(self, ctx:xamaParser.LvContext):
-        pass
-
-
+        id_ = ctx.ID().getText()
+        if not symbol_table.lookup(id_):
+            print(f"Erro: Variável ou função '{id_}' não declarada")
 
 del xamaParser
--- driver.py
+++ driver.py
@@ -2,6 +2,7 @@ import sys
 from antlr4 import *
 from AnalisadorSintatico.XamaLexer import xamaLexer
 from AnalisadorSintatico.xamaParser import xamaParser
+from AnalisadorSintatico.xamaListener import xamaListener
 
 def main(argv):
     input_stream = FileStream(argv[1])
@@ -11,6 +12,10 @@ def main(argv):
     
     tree = parser.program()
     #print(tree.toStringTree(recog=parser))
+    
+    listener = xamaListener()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
 
 if __name__ == '__main__':
     main(sys.argv)
